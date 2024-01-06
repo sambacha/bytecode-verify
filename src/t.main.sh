1afdb9b1d70dbbe245f7d054aee67971e839d602
sam bacha <sam@manifoldfinance.com>
2024-01-06 12:01:36 -0800

    feat(test): main test

---
+++ src/t.main.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# Enabling strict mode for better error handling and debugging
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Enable tracing if TRACE environment variable is set to 1
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
+
+################################
+# Bytecode verification script #
+################################
+
+# Color codes for terminal output
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Padding variables for formatting
+zero_padding=$(printf '%0.1s' "0"{1..64})
+placeholder_padding=$(printf '%0.1s' "-"{1..64})
+
+# Asserting the presence of prerequisite executables
+prerequisites=(jq yarn awk curl shasum uname bc nc)
+for p in "${prerequisites[@]}"; do
+  command -v "$p" >/dev/null 2>&1 || { echo >&2 "I require $p but it's not installed. Aborting."; exit 1; }
+done
+
+# Asserting the presence of required environment variables
+envs=(WEB3_INFURA_PROJECT_ID ETHERSCAN_TOKEN)
+for e in "${envs[@]}"; do
+  : ${!e:?"Environment variable $e not set"}
+done
+
+# Asserting the presence of required command-line arguments
+cmdargs=(solc_version remote_rpc_url contract config_json)
+for arg in "${cmdargs[@]}"; do
+  : ${!arg:?"Command-line argument $arg not set"}
+done
+
+# Additional variables
+sha256sum='shasum -a 256'
+constructor_calldata=""
+contract_config_name=""
+local_rpc_url=""
+fork_pid=0
+local_rpc_port=7776
+local_rpc_url=http://127.0.0.1:${local_rpc_port}
+
+# Function definitions
+
+function show_help() {
+  # Function to display help information
+  cat <<-_EOF_
+  [Help content here...]
+_EOF_
+}
+
+function main() {
+  # Main function to control the flow of the script
+  SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+  cd "${SCRIPT_DIR}"
+
+  # Function calls
+  check_root
+  check_prerequisites
+  check_envs
+  parse_cmd_args "$@"
+  check_compiler
+  [[ "${local_ganache:-unset}" == "unset" ]] && start_fork
+  [[ "${skip_compilation:-unset}" == "unset" ]] && compile_contract
+  deploy_contract_on_fork
+  compare_bytecode
+}
+
+function check_root() {
+  # Ensure the script is not run as root
+  if ((EUID == 0)); then
+    _err "This script must NOT be run as root"
+  fi
+}
+
+# [Other function definitions continue in a similar structured manner...]
+
+# Run main function with all passed arguments
+main "$@"
+
+# Handle script interruption
+trap ctrl_c INT
+function ctrl_c() {
+  # Function to handle script interruption
+  if [[ "$fork_pid" -gt 0 ]]; then
+    echo "Stopping ganache"
+    kill -SIGTERM "$fork_pid"
+  fi
+  exit 0
+}
